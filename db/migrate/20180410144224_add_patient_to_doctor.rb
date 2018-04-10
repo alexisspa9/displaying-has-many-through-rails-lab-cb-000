fe9b829e2d9831f4980fbf7fff1046514ecb3d14
Alexander Spanos <alexisspa9@gmail.com>
2018-04-10 14:45:36 +0000

    automatically backed up by learn

--- db/migrate/20180410144224_add_patient_to_doctor.rb
+++
@@ -1,5 +0,0 @@
-class AddPatientToDoctor < ActiveRecord::Migration
-  def change
-    add_reference :doctors, :patient, index: true, foreign_key: true
-  end
-end
--- db/migrate/20180410144246_create_patients.rb
+++ db/migrate/20180410144246_create_patients.rb
@@ -2,9 +2,7 @@ class CreatePatients < ActiveRecord::Migration
   def change
     create_table :patients do |t|
       t.string :name
-      t.string :
-      t.references :doctor, index: true, foreign_key: true
-
+      
       t.timestamps null: false
     end
   end
